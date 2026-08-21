85ba221716a2d35e1cdaaf5aa882825f6d4c080f
HuJia(Mac) <1946486286@qq.com>
2026-08-21 18:32:57 +0800

    lint, dast, sast

--- scripts/jshint-script.sh
+++ scripts/jshint-script.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-jshint --exclude="node_modules/" --reporter=unix . > JSHint-report
+jshint --exclude="node_modules/" --reporter=unix > JSHint-report
 echo $? > /dev/null
--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+docker run -v $(pwd):/zap/wrk/:rw -i zaproxy/zap-stable zap-baseline.py -t "https://lightning-potato.github.io/bitcoin-order-app/" -r zap_baseline_report.html
 echo $? > /dev/null
